333d7f7eec18b408d9f34db92bafd0ed6c74ad92
Brandon Horst <brandonhorst@gmail.com>
2016-09-21 09:47:23 -0400

    Add onTriggerAfterActivate function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import {concat} from 'rxjs/operator/concat'
 import {mergeMap} from 'rxjs/operator/mergeMap'
+import {switchMap} from 'rxjs/operator/switchMap'
+import {first} from 'rxjs/operator/first'
 import {startWith} from 'rxjs/operator/startWith'
 import {fromPromise} from 'rxjs/observable/fromPromise'
+import {Observable} from 'rxjs/Observable'
+import {of as ofObservable} from 'rxjs/observable/of'
 
 export function onActivate (promiseFunction, initial = null, clear) {
   return {
@@ -16,10 +20,31 @@ export function onActivate (promiseFunction, initial = null, clear) {
   }
 }
 
-export function onFetch (promiseFunction, initial = null, clear) {
+export function onFetch (promiseFunction, initial = null, clear = true) {
   return {
     fetch ({props}) {
       return fromPromise(promiseFunction(props))::startWith(initial)
+    },
+    clear
+  }
+}
+
+export function onTriggerAfterActivate (promiseFunction, initial = null, clear) {
+  return {
+    fetch ({activate, props}) {
+      let tapObserver
+      const tap = new Observable(observer => { tapObserver = observer })
+      const trigger = () => tapObserver.next()
+
+      return ofObservable(true)
+        ::concat(activate)
+        ::mergeMap(() => {
+          return tap::first()
+        })
+        ::switchMap(() => {
+          return promiseFunction(props).then((data) => ({trigger, data}))
+        })
+        ::startWith({trigger, data: initial})
     },
     clear
   }
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 import {fromPromise} from 'rxjs/observable/fromPromise'
 
-import {onActivate, onFetch} from '../src'
+import {onActivate, onFetch, onTriggerAfterActivate} from '../src'
 import sinon, {spy} from 'sinon'
 
 describe('lacona-source-helpers', () => {
@@ -56,6 +56,50 @@ describe('lacona-source-helpers', () => {
         resultSpy.withArgs(2),
         resultSpy.withArgs(1)
       )
+    })
+  })
+
+  describe('onTriggerAfterActivate', () => {
+    it('calls promiseFunction on the first trigger call after activation', (done) => {
+      const promiseSpy = spy(function promise() {})
+      const resultSpy = spy(function result() {})
+      const activateSpy = spy(function activate() {})
+      const triggerSpy = spy(function trigger() {})
+
+      function promiseFunction (props) {
+        promiseSpy()
+        return Promise.resolve(props)
+      }
+
+      const source = onTriggerAfterActivate(promiseFunction, 2)
+
+      const activate = fromPromise(new Promise((resolve, reject) => {
+        setTimeout(() => {
+          activateSpy()
+          resolve()
+        }, 0)
+      }))
+
+      const observable = source.fetch({activate, props: 1})
+      observable.forEach(({trigger, data}) => {
+        resultSpy(data)
+        if (triggerSpy.callCount === 0) {
+          setTimeout(() => {
+            triggerSpy()
+            trigger()
+          }, 0)
+        } else {
+          sinon.assert.callOrder(
+            resultSpy.withArgs(2),
+            activateSpy,
+            triggerSpy,
+            promiseSpy,
+            resultSpy.withArgs(1)
+          )
+          done()
+        }
+      })
+
     })
   })
 })
